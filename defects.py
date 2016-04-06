b0ec38b1d75c627d6d44a8ec7b3f9dc9420ab4fa
rahlk <i.m.ralk@gmail.com>
2016-04-05 20:15:29 -0400

    autocommit

--- defects.py
+++ defects.py
@@ -1,11 +1,14 @@
 from __future__ import print_function, division
 
-from pdb import set_trace
 from os import remove
+
+import numpy as np
+
 from Oracle.Prediction import Bugs, rforest
 from Oracle.methods1 import explore, createTbl
 from Planners.xtree import xtree
 from tools.sk import rdivDemo
+from tools.stats import ABCD
 
 __author__ = 'rkrsn'
 
@@ -15,18 +18,35 @@ def improve():
     train, test = explore(dir='Data/Jureczko/')
     for a, b in zip(train, test):
         me = [b[0].split('/')[-2]]
-        for _ in xrange(1):
+        for _ in xrange(10):
             new = xtree(train=a, test=b)
             new.to_csv('new.csv', index=False)
             rfTrain = createTbl(a)
             rfTest = createTbl(['new.csv'])
             actual = Bugs(createTbl(b))
             imporved = rforest(train=rfTrain, test=rfTest)
-            gain = float("%0.2f"%((1 - sum(imporved) / sum(actual)) * 100))
+            gain = float("%0.2f" % ((1 - sum(imporved) / sum(actual)) * 100))
             me.append(gain)
             remove('new.csv')
         all.append(me)
     rdivDemo(all, isLatex=False, globalMinMax=True, high=100, low=0)
+
+
+def test_oracle():
+    E = []
+    train, test = explore(dir='Data/Jureczko/')
+    for a, b in zip(train, test):
+        me = [b[0].split('/')[-2]]
+        for _ in xrange(10):
+            rfTrain = createTbl(a)
+            rfTest = createTbl(b)
+            actual = Bugs(b)
+            prdctd = rforest(train=rfTrain, test=rfTest)
+            abcd = ABCD(before=actual, after=prdctd)
+            ED = np.array([k.stats()[-1] for k in abcd()])
+            me.append(ED[1])
+        E.append(me)
+    rdivDemo(E, isLatex=False, globalMinMax=True, high=100, low=0)
 
 
 if __name__ == '__main__':
--- tools/stats.py
+++ tools/stats.py
@@ -1,47 +1,50 @@
 from __future__ import division
-from pdb import set_trace
-
-class counter():
-  def __init__(self, before, after, indx):
-    self.indx = indx
-    self.actual = before
-    self.predicted = after
-    self.TP, self.TN, self.FP, self.FN = 0, 0, 0, 0
-    for a, b in zip(self.actual, self.predicted):
-      if a == indx and b == indx:
-        self.TP += 1
-      elif a == b and a != indx:
-        self.TN += 1
-      elif a != indx and b == indx:
-        self.FP += 1
-      elif a == indx and b != indx:
-        self.FN += 1
-      elif a != indx and b != indx:
-        pass
-  def stats(self):
-    try:
-      Sen = self.TP / (self.TP + self.FN)
-      Spec = self.TN / (self.TN + self.FP)
-      Prec = self.TP / (self.TP + self.FP)
-      Acc = (self.TP + self.TN) / (self.TP + self.FN + self.TN + self.FP)
-      F = 2 * (Prec*Sen) / (Prec+Sen)
-      F1 = 2 * self.TP / (2 * self.TP + self.FP + self.FN)
-      G = 2 * Sen * Spec / (Sen + Spec)
-      G1 = Sen * Spec / (Sen + Spec)
-      return Sen, Spec, Prec, Acc, F, G
-    except ZeroDivisionError:
-      return 0, 0, 0, 0, 0, 0
+import numpy as np
 
 
-class ABCD():
+class Counter:
+    def __init__(self, before, after, indx):
+        self.indx = indx
+        self.actual = before
+        self.predicted = after
+        self.TP, self.TN, self.FP, self.FN = 0, 0, 0, 0
+        for a, b in zip(self.actual, self.predicted):
+            if a == indx and b == indx:
+                self.TP += 1
+            elif a == b and a != indx:
+                self.TN += 1
+            elif a != indx and b == indx:
+                self.FP += 1
+            elif a == indx and b != indx:
+                self.FN += 1
+            elif a != indx and b != indx:
+                pass
 
-  "Statistics Stuff, confusion matrix, all that jazz..."
+    def stats(self):
+        try:
+            Sen = self.TP / (self.TP + self.FN)
+            Spec = self.TN / (self.TN + self.FP)
+            Prec = self.TP / (self.TP + self.FP)
+            Acc = (self.TP + self.TN) / (self.TP + self.FN + self.TN + self.FP)
+            F = 2 * (Prec * Sen) / (Prec + Sen)
+            G = 2 * Sen * Spec / (Sen + Spec)
+            # F1 = 2 * self.TP / (2 * self.TP + self.FP + self.FN)
+            # G1 = Sen * Spec / (Sen + Spec)
+            ED = np.sqrt(0.6*(1-Sen)**2+0.3*(1-Spec)**2)
+            return Sen, Spec, Prec, Acc, F, G, ED
+        except ZeroDivisionError:
+            return 0, 0, 0, 0, 0, 0
 
-  def __init__(self, before, after):
-    self.actual = before
-    self.predicted = after
 
-  def __call__(self):
-    uniques = set(self.actual)
-    for u in list(uniques):
-      yield counter(self.actual, self.predicted, indx=u)
+class ABCD:
+    """ Statistics Stuff, confusion matrix, all that jazz...
+    """
+
+    def __init__(self, before, after):
+        self.actual = before
+        self.predicted = after
+
+    def __call__(self):
+        uniques = set(self.actual)
+        for u in list(uniques):
+            yield Counter(self.actual, self.predicted, indx=u)
